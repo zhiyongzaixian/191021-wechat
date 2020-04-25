4612cad2ab5d409654f7a8c67aaa4112c7b51106
zhiyongzaixian <13852292775@163.com>
2020-04-25 16:34:40 +0800

    'day04'

--- guiguyinyue_demo/pages/video/video.js
+++ guiguyinyue_demo/pages/video/video.js
@@ -1,17 +1,28 @@
-// pages/video/video.js
+import request from '../../utils/request'
 Page({
-
-  /**
-   * 页面的初始数据
-   */
   data: {
-
+    videoGroupList:[], // 导航列表
+    navId: '', // 视频标签id标识
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
-  onLoad: function (options) {
+  onLoad: async function (options) {
+    // 获取导航列表数据
+    let videoGroupListData = await request('/video/group/list');
+    this.setData({
+      videoGroupList: videoGroupListData.data.slice(0, 14),
+      navId: videoGroupListData.data[0].id
+    })
+  },
+  // 修改导航id值
+  changeNavId(event){
+    console.log(event);
+    console.log(typeof event.currentTarget.id);
+    this.setData({
+      navId: event.currentTarget.id>>>0
+    })
   },
 
   /**
---
+++ guiguyinyue_demo/test/test.js
@@ -0,0 +1,12 @@
+// 位移运算符 >>> <<<
+
+
+let num = 3;
+// 0000 0011
+// 00000 001
+console.log(num >>> 2); // 0
+console.log(num >>> 1); // 1
+
+
+console.log(!!num);
+console.log(true>>>0); // 将非number值转换成绝对number值

c006f1b8d0af051686bd312a83fa7c0a5190d3b8
zhiyongzaixian <13852292775@163.com>
2020-04-30 15:46:40 +0800

    'day08'

---
+++ uniapp_wangyiyanxuan/uniapp-硅谷商城/main.js
@@ -0,0 +1,28 @@
+import Vue from 'vue'
+import App from './App'
+
+// 关闭Vue开发的提示
+Vue.config.productionTip = false
+
+// mp: mini programe
+// mpType： 用于声明小程序的类型
+// 声明App组件代表整个小程序应用
+App.mpType = 'app' // application
+
+const app = new Vue({
+    ...App
+})
+
+// 挂载应用，等同于原生小程序的App()
+app.$mount()
+
+
+// new Vue({
+// 	el: '#app',
+// 	render: h => h(App)
+// })
+
+// new Vue({
+// 	el: '#app',
+// 	render: h => h(App)
+// }).$mount()

46fda42fce5fd35db50289ee6cf21a49316b95dd
zhiyongzaixian <13852292775@163.com>
2020-04-30 17:27:00 +0800

    'day08'

---
+++ uniapp_server/server.js
@@ -0,0 +1,35 @@
+let Koa = require('koa');
+let KoaRouter = require('koa-router');
+
+
+// 1. 生成项目的应用
+const app = new Koa();
+// 2. 生成路由器
+const router = new KoaRouter();
+
+// express: req === request(请求信息), res === response(响应信息对象),
+// 注册测试路由
+router.get('/test1', (ctx, next) => {
+	// koa: ctx === req + res的角色
+	// express： res.send = '返回的数据'
+	// koa: ctx.body = '返回的数据'
+	ctx.body = '测试返回的数据'
+});
+
+let indexData = require('./datas/index.json');
+router.get('/getIndexData', (ctx, next) => {
+	ctx.body = indexData
+})
+
+
+
+// 3. 声明使用中间键
+app
+	.use(router.routes()) // 使用路由
+	.use(router.allowedMethods()) // 使用路由方法
+	
+// 4. 监听端口号
+app.listen('4001', () => {
+	console.log('服务器启动成功');
+	console.log('服务器地址: http://localhost:4001')
+});
--- uniapp_wangyiyanxuan/uniapp-硅谷商城/pages/index/index.vue
+++ uniapp_wangyiyanxuan/uniapp-硅谷商城/pages/index/index.vue
@@ -1,6 +1,13 @@
 <template>
-	<view>
-		
+	<view id="indexContainer">
+		<view class="header">
+			<image class="logo" src="/static/images/logo.png" mode=""></image>
+			<view class="searchInput">
+				<text class="iconfont icon-sousuo"></text>
+				<input type="text" value="" placeholder="搜索商品" placeholder-class="placeholder"/>
+			</view>
+			<button>登录登录</button>
+		</view>
 	</view>
 </template>
 
@@ -15,5 +22,61 @@
 </script>
 
 <style lang="stylus">
+	#indexContainer
+		.header 
+			display flex
+			height 60upx
+			width 100%
+			padding 10upx 0
+			.logo 
+				width 140upx
+				height 40upx
+				margin 10upx 30upx
+			.searchInput
+				position relative
+				height 60upx
+				width 420upx
+				background #ededed
+				.iconfont 
+					position absolute
+					left 10upx
+					top 15upx
+					font-size 30upx
+				input
+					width 370upx
+					margin-left 50upx
+					height 100%
+					color #BB2C08
+				.placeholder
+					font-size 28upx
+					text-align center
+					line-height 60upx
+			button
+				font-size 24upx
+				width 144upx
+				height 60upx
+				margin 0 10upx
+				color #b4282d
+				white-space nowrap
+				overflow hidden
+				text-overflow ellipsis
+				padding 0 5upx
+				
 	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	.testxxxxxxxxxxxxxxxxxxxxxxxxxxxx
+		font-size 0
 </style>
